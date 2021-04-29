7a370c15b835027b946bb0ce0db9a18cd8e18e0f
aelbakry <ahmed.sherif1999@hotmail.com>
2021-04-29 22:42:38 +0200

    bug fixes

--- senet.py
+++ senet.py
@@ -8,6 +8,8 @@ import math
 
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
+
 from torch.utils import model_zoo
 
 #from sync_batchnorm import SynchronizedBatchNorm2d as BatchNorm2d
@@ -87,6 +89,59 @@ pretrained_settings = {
     },
 }
 
+class ArcFaceLoss(nn.modules.Module):
+    def __init__(self,s=30.0,m=0.5):
+        super(ArcFaceLoss, self).__init__()
+        self.classify_loss = nn.CrossEntropyLoss()
+        self.s = s
+        self.easy_margin = False
+        self.cos_m = math.cos(m)
+        self.sin_m = math.sin(m)
+        self.th = math.cos(math.pi - m)
+        self.mm = math.sin(math.pi - m) * m
+
+    def forward(self, logits, labels, epoch=0):
+        cosine = logits
+        sine = torch.sqrt(1.0 - torch.pow(cosine, 2))
+        phi = cosine * self.cos_m - sine * self.sin_m
+        if self.easy_margin:
+            phi = torch.where(cosine > 0, phi, cosine)
+        else:
+            phi = torch.where(cosine > self.th, phi, cosine - self.mm)
+
+        one_hot = torch.zeros(cosine.size(), device='cuda')
+        one_hot.scatter_(1, labels.view(-1, 1).long(), 1)
+        # -------------torch.where(out_i = {x_i if condition_i else y_i) -------------
+        output = (one_hot * phi) + ((1.0 - one_hot) * cosine)
+        output *= self.s
+        loss1 = self.classify_loss(output, labels)
+        loss2 = self.classify_loss(cosine, labels)
+        gamma=1
+        loss=(loss1+gamma*loss2)/(1+gamma)
+        return loss
+
+class ArcMarginProduct(nn.Module):
+    r"""Implement of large margin arc distance: :
+        Args:
+            in_features: size of each input sample
+            out_features: size of each output sample
+            s: norm of input feature
+            m: margin
+            cos(theta + m)
+        """
+    def __init__(self, in_features, out_features):
+        super(ArcMarginProduct, self).__init__()
+        self.weight = nn.Parameter(torch.FloatTensor(out_features, in_features))
+        # nn.init.xavier_uniform_(self.weight)
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        stdv = 1. / math.sqrt(self.weight.size(1))
+        self.weight.data.uniform_(-stdv, stdv)
+
+    def forward(self, features):
+        cosine = F.linear(F.normalize(features), F.normalize(self.weight.cuda()))
+        return cosine
 
 class SEModule(nn.Module):
 
@@ -547,7 +602,7 @@ class SENetArcFace(nn.Module):
         num_classes (int): Number of outputs in `last_linear` layer.
             - For all models: 1000
         """
-        super(SENet, self).__init__()
+        super(SENetArcFace, self).__init__()
         self.inplanes = inplanes
         if input_3x3:
             layer0_modules = [
@@ -625,7 +680,6 @@ class SENetArcFace(nn.Module):
         self.relu = nn.ReLU(inplace=True)
         self.fc2 = nn.Linear(512 * self.EX, 512)
         self.bn3 = nn.BatchNorm1d(512)
-        self.extract_feature = extract_feature
 
         self.arc_margin_product=ArcMarginProduct(512, 2)
 
